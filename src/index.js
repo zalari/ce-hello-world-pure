b34a832aae86e895ee26a7789d0d306a8765602b
Christian Ulbrich <christian.ulbrich@zalari.de>
2020-02-03 23:32:02 +0100

    very initial pure component!

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// we are so klever
+// -> https://vcfvct.wordpress.com/2016/07/31/js-tilde-iief/
+
+~function () {
+
+  // define the actual class
+  const Z4iHelloWorld = class extends HTMLElement {
+
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+      console.log('I am there!', this);
+      this.innerHTML = this.render();
+    }
+
+    disconnectedCallback() {
+      console.log('I am not there');
+    }
+
+    render() {
+      return `<div>Hello World!</div>`;
+    }
+
+  };
+
+  customElements.define('z4i-hello-world', Z4iHelloWorld);
+
+
+}();
